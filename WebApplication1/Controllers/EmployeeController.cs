387bb9aa14a53c75935a0f049b7ee37fc2366a46
Namra  Abid <s.namraabid@gmail.com>
2023-04-14 20:17:11 +0500

    Sending Data From URL to Controller

---
+++ WebApplication1/Controllers/EmployeeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public string EmployeeProfile(int id)
+        {
+            string profile;
+            // string profile =sring.Empty
+            if (id == 1)
+            {
+                profile = "Employee 1";
+            }
+            else if (id == 2)
+            {
+                profile = "Employee 2";
+            }
+            else
+            {
+                profile = "No record Found !";
+            }
+            return profile;
+        }
+
+    }
+}
---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+       public string Index()
+        {
+            return "Hello World:";
+        }
+        public string Name()
+        {
+            return "Hello Namra:";
+        }
+    }
+}
