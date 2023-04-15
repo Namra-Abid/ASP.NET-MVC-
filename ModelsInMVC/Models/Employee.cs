22065721a0ad053b8af2a01d9ad3aa35796f245b
Namra  Abid <s.namraabid@gmail.com>
2023-04-15 19:27:12 +0500

    Created Employee Model and Displayed it

---
+++ ModelsInMVC/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ModelsInMVC.Models
+{
+    public class Employee
+    {
+        public int id;
+        public string name;
+        public string email;
+         
+        public  Employee GetEmployee()
+        {
+            Employee emp = new Employee() { id=1, name="Namra", email="namra@xxx.com"};
+            return emp;
+        }
+
+    }
+}
