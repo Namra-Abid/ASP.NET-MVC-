1e6173499cf4b4553c7ad8f9047ce38526e3e345
Namra  Abid <s.namraabid@gmail.com>
2023-04-15 17:14:33 +0500

    New Faculty Controller and Function with View()

---
+++ WebApplication1/Controllers/FacultyController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class FacultyController : Controller
+    {
+        // GET: Faculty
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Faculty/Details/5
+        public ActionResult Details()
+        {
+            return View();
+        }
+
+        
+
+        
+    }
+}
